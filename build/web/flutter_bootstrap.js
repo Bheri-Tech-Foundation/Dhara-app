ec24b49b8ec4135036eb5245a3e298d0146c2bb8
Trupti Gavit <163740738+TruptiGavit@users.noreply.github.com>
2025-11-28 16:22:23 +0530

    fixed overflow issue

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2621055574"
+    serviceWorkerVersion: "3660378878"
   }
 });
